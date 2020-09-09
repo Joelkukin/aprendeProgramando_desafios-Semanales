f56a0f52863dc64294102b80602113dba1d97078
Joelkukin <joelkukin@gmail.com>
2020-09-09 16:52:13 +0000

    add desafioSemanal#2

---
+++ desafio-semanal-#2/main.js
@@ -0,0 +1,7 @@
+var nombre = "joel"; // COMPLETAR//
+var edad = "18"; // COMPLETAR//
+var actividad = "programar"; //COMPLETAR//
+var actividad2 = "call of duty"; //COMPLETAR//
+
+var frase = "Hola mi nombres es " +  nombre +  " y tengo " +  edad +  " años, me gusta " +  actividad +  " y jugar a " +  actividad2 + ".";
+console.log(frase);
